3683c8e7d8c5d83a56b2d3a4e05da5f97b915072
github username <github email address>
2020-01-07 14:27:48 -0700

    completed palindromes

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,27 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// solution two - maybe not ideal because don't have to compare every element but only up to the center one but this double compares. 
+function palindrome(str) {
+    // i (index), need to subtract index and minus 1 from str.length to compare to correct Element Because str.length gives overall length and arrays index start at 0 
+    // example str = 'abcba'
+    // str.length = 5 
+    // you need to compare first letter with last, so 5 -0 -1 = 4 
+    // compare first letter with letter that has index of 4 which is the last letter
+    return str.split('').every((char, i) => {
+        return char === str[str.length -i -1]
+    })
+}
+
+
+// solution one
+// function palindrome(str) {
+//     const reversedString = str
+//     .split('')
+//     .reverse()
+//     .join('')
+
+//     return str === reversedString
+// }
 
 module.exports = palindrome;
