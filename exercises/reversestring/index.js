1bda898a6783c5bddd3baa1bced057bd91c81616
github username <github email address>
2020-01-07 13:40:55 -0700

    completed reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,37 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// reduce option
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed
+  }, '')
+}
+
+// for loop option
+function reverse(str) {
+    let reversed = ''
+    // newer way of doing for loop same as: 
+    // for(i=0; i<str.length; i++){
+    //     reversed = str.charAt(i) + reversed
+    // }
+    for(let character of str){
+        reversed = character + reversed
+    }
+     
+    return reversed
+}
+
+
+// .reverse option
+
+function reverse(str) {
+    return str
+        .split('')
+        .reverse()
+        .join('')
+}
+
 
 module.exports = reverse;
+
