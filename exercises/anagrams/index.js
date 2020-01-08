a036da9da2a9fa1878abdb68613f3ebe278df81a
github username <github email address>
2020-01-07 21:19:35 -0700

    completed anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,47 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+
+// second option with sort
+function anagrams(stringA, stringB) {
+   return cleanString(stringA) === cleanString(stringB)
+}
+
+function cleanString(str){
+    return str
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('')
+}
+
+// first solution option
+function anagrams(stringA, stringB) {
+    const aCharMap = buildCharMap(stringA)
+    const bCharMap = buildCharMap(stringB)
+
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+        return false
+    } 
+
+    for(let char in aCharMap){
+        if(aCharMap[char] !== bCharMap[char]){
+            return false
+        }
+    }
+
+    return true
+}
+
+function buildCharMap(str){
+    const charMap = {}
+
+    for(let char of str.replace(/[^\w]/g, '').toLowerCase()){
+        charMap[char] = charMap[char] + 1 || 1
+    }
+
+    return charMap
+}
 
 module.exports = anagrams;
