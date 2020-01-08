a69d49fa34fc1232d35b42808b849630bc8ab70b
github username <github email address>
2020-01-07 17:58:37 -0700

    completed maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,35 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let charMap = {}
+
+    // when we interate through charMap if a value if bigger than 0 then we set that = to max, and set maxChar = to the key that holds that value
+    let max = 0
+    let maxChar = ''
+
+    // for(let char of str){
+    //     if(charMap[char]){
+    //         chars[char]++
+    //     } else {
+    //         charMap[char] = 1
+    //     }
+    // }
+
+    // same as above
+    for(let char of str){
+        charMap[char] = charMap[char] + 1 || 1
+    }
+
+    // use FOR IN instead of FOR OF (above) because interating through an Object. FOR OF interates through string or Array. (way to remember: object starts with O and ironically we don't use for OF we use for IN) 
+    // char in example below is the key, not the value 
+    for(let char in charMap){
+        if(charMap[char] > max){
+            max = charMap[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
 
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,21 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+   const reversed = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('')
+//    if(n <0){
+//     return parseInt(reversed) * -1
+//    }
+//    return parseInt(reversed)
+
+// this is the same as above code because Math.sign returns positive 1 if its a postive Number, -1 if its a negative Number, or zero if its zero
+    return parseInt(reversed) * Math.sign(n)
+
+}
+
 
 module.exports = reverseInt;
